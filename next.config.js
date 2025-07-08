ebf324c2060c6f8f739c7a3f879761fa529d487e
Marcus Little <mcolelittle@gmail.com>
2025-07-08 18:18:58 -0400

    add custom headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';base-uri 'self';form-action 'self';",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
