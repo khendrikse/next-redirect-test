82ee7703c3568382fb0991200b4020e417f26279
Karin <=>
2025-07-11 17:11:05 +0200

    remove extra plugin

--- plugins/redirect-plugin/index.js
+++
@@ -1,8 +0,0 @@
-export const onPreBuild = function({netlifyConfig}) {
-    netlifyConfig.redirects.push({
-        from: "/test",
-        to: "/image-cdn",
-        status: 301,
-        force: true,
-      });
-  }
